bcc7e5f8506d5b42d2fd556a154f910f299826ad
蔡政諺 <henrytsai88@gmail.com>
2024-03-27 03:59:25 +0800

    initial commit

---
+++ 1/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> hashTable;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hashTable.count(complement)) {
+                return {hashTable[complement], i};
+            }
+            hashTable[nums[i]] = i;
+        }
+        return {};
+    }
+};
