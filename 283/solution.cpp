f82ab7934d9928d8ce1f15ecad0c500726803251
蔡政諺 <henrytsai88@gmail.com>
2024-04-01 19:33:50 +0800

    add problem 283

---
+++ 283/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for (int num : nums) if(num != 0) nums[i++] = num;
+        for (; i < nums.size(); i++) nums[i] = 0;
+    }
+};
