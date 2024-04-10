7816d83a92428842f1ed2eea3dc7bce64471f329
蔡政諺 <henrytsai88@gmail.com>
2024-04-10 17:21:19 +0800

    add problem 33

---
+++ 33/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else right = mid;
+        }
+        int k = left;
+        if (target > nums[n - 1]) left = 0, right = k - 1;
+        else left = k, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+};
