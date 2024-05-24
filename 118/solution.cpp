6ca2792a0f3d097a3d262e9c132b0cb673dd7bbf
蔡政諺 <henrytsai88@gmail.com>
2024-05-25 00:46:52 +0800

    add problem 118

---
+++ 118/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for (int i = 0; i < numRows; i++) {
+            vector<int> v(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                v[j] = res.back()[j - 1] + res.back()[j];
+            }
+            res.push_back(v);
+        }
+        return res;
+    }
+};
