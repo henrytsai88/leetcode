94b0dac18d3349ce8823c3d2f8f16dac6f9ad081
蔡政諺 <henrytsai88@gmail.com>
2024-05-27 22:34:43 +0800

    add problem 1143

---
+++ 1143/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<int> pre(n + 1, 0), cur(n + 1, 0);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1[i - 1] == text2[j - 1]) cur[j] = 1 + pre[j - 1];
+                else cur[j] = max(cur[j - 1], pre[j]);
+            }
+            swap(pre, cur);
+        }
+        return pre[n];
+    }
+};
