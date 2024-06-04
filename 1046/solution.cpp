936964d3c26eec9249c55c4a318fb9f8073c673b
蔡政諺 <henrytsai88@gmail.com>
2024-06-04 16:58:58 +0800

    add problem 1046

---
+++ 1046/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for (int stone : stones) pq.push(stone);
+        while (pq.size() > 1) {
+            int first = pq.top(); pq.pop();
+            int second = pq.top(); pq.pop();
+            pq.push(first - second);
+        }
+        return pq.top();
+    }
+};
