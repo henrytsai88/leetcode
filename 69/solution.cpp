a494f6e54bb3c9f2c7e524e89bddcda22c1b46b5
蔡政諺 <henrytsai88@gmail.com>
2024-07-27 22:51:22 +0800

    add problem 69

---
+++ 69/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        int left = 1, right = x, res;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                res = mid;
+            }
+            else right = mid - 1;
+        }
+        return res;
+    }
+};
